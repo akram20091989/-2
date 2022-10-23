fecc7583aa9f8899f5081a47a0c25685be6acafd
akram <akram20091989@gmail.com>
2022-10-23 08:24:56 +0600

    figs

--- домашка №2/main.swift
+++ домашка №2/main.swift
@@ -45,46 +45,46 @@ weather(weather: city5)
 let city6 = readLine()!
 weather(weather: city6)
 
-//2-ое задание магазин
-
-print("В нашем магазине есть:")
-print("Кола - 99сом, Кефир - 60сом, Молоко - 55сом, Хлеб - 25сом , Вода - 20сом")
-
-let cola = 99
-let kefir = 60
-let moloko = 55
-let hleb = 25
-let voda = 20
-var total = 0
-
-print("Выберите товар")
-let tovary = readLine()!
-
-func sale(tovary : String){
-    switch tovary {
-    case "cola":
-        print("cola - \(cola)")
-    case "kefir":
-        print("kefir - \(kefir)")
-    case "moloko":
-        print("moloko - \(moloko)")
-    case "hleb":
-        print("hleb - \(hleb)")
-    case "voda":
-        print("voda - \(voda)")
-    default:
-        print("Выберите другой товар")
-    }
-}
-let tovar = readLine()!
-sale(tovary: tovar)
-let tovar2 = readLine()!
-sale(tovary: tovar2)
-let tovar3 = readLine()!
-sale(tovary: tovar3)
-let tovar4 = readLine()!
-sale(tovary: tovar4)
-let tovar5 = readLine()!
-sale(tovary: tovar5)
-
-
+////2-ое задание магазин
+//
+//print("В нашем магазине есть:")
+//print("Кола - 99сом, Кефир - 60сом, Молоко - 55сом, Хлеб - 25сом , Вода - 20сом")
+//
+//let cola = 99
+//let kefir = 60
+//let moloko = 55
+//let hleb = 25
+//let voda = 20
+//var total = 0
+//
+//print("Выберите товар")
+//let tovary = readLine()!
+//
+//func sale(tovary : String){
+//    switch tovary {
+//    case "cola":
+//        print("cola - \(cola)")
+//    case "kefir":
+//        print("kefir - \(kefir)")
+//    case "moloko":
+//        print("moloko - \(moloko)")
+//    case "hleb":
+//        print("hleb - \(hleb)")
+//    case "voda":
+//        print("voda - \(voda)")
+//    default:
+//        print("Выберите другой товар")
+//    }
+//}
+//let tovar = readLine()!
+//sale(tovary: tovar)
+//let tovar2 = readLine()!
+//sale(tovary: tovar2)
+//let tovar3 = readLine()!
+//sale(tovary: tovar3)
+//let tovar4 = readLine()!
+//sale(tovary: tovar4)
+//let tovar5 = readLine()!
+//sale(tovary: tovar5)
+//
+//
